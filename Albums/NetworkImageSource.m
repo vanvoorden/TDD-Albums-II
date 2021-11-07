ad426129365c61dd86f9a94d9cf60d64c83e5b5f
Frederik Van Voorden <rick@northbronson.com>
2021-11-07 12:33:48 -0800

    Network Image Source

---
+++ Albums/NetworkImageSource.m
@@ -0,0 +1,45 @@
+//
+//  NetworkImageSource.m
+//  Albums
+//
+//  Copyright © 2021 North Bronson Software
+//
+//  This Item is protected by copyright and/or related rights. You are free to use this Item in any way that is permitted by the copyright and related rights legislation that applies to your use. In addition, no permission is required from the rights-holder(s) for scholarly, educational, or non-commercial uses. For other uses, you need to obtain permission from the rights-holder(s).
+//
+//  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+#import "NetworkImageSource.h"
+
+@implementation NetworkImageSource
+
+@end
+
+@implementation NetworkImageSource (CreateImageSource)
+
++ (CGImageSourceRef _Nullable (*_Nonnull)(CFDataRef _Nonnull, CFDictionaryRef _Nullable))createImageSource {
+  return CGImageSourceCreateWithData;
+}
+
++ (CGImageSourceRef)createImageSourceWithData:(CFDataRef)data
+                                      options:(CFDictionaryRef)options {
+  return [self createImageSource](data, options);
+}
+
+@end
+
+@implementation NetworkImageSource (CreateImage)
+
++ (CGImageRef _Nullable (*_Nonnull)(CGImageSourceRef _Nonnull, size_t, CFDictionaryRef _Nullable))createImage {
+  return CGImageSourceCreateImageAtIndex;
+}
+
++ (CGImageRef)createImageWithImageSource:(CGImageSourceRef)imageSource
+                                 atIndex:(size_t)index
+                                 options:(CFDictionaryRef)options {
+  return [self createImage](imageSource, index, options);
+}
+
+@end
